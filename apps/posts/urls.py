c73b2c3a8fdf18bb8cb400497f17c8c2afd2b703
bagget777 <123551503+bagget777@users.noreply.github.com>
2023-08-22 20:04:42 +0600

    commit3

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -1,7 +1,8 @@
-from .views import IndexView, AboutView
+from .views import IndexView, AboutView, FAQView
 from django.urls import path
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("about/", AboutView.as_view(), name="about"),
+    path("faq/", FAQView.as_view(), name="faq")
 ]
--- apps/posts/views.py
+++ apps/posts/views.py
@@ -2,14 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse, request
 from django.views import generic
 from .models import *
+from django.views.generic import TemplateView
 
-class IndexView(request):
+class IndexView(generic.ListView):
     model = Post
     template_name = "index.html"
     context_object_name = "posts"
 
-class AboutView(request):
+class AboutView(TemplateView):
     template_name = 'posts/About.html'
+    
+class FAQView(TemplateView):
+    template_name = 'posts/faq.html'
+
 
 
 
